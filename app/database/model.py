b14e773cb4cc655a207a0e417ca20e92b55b63e4
abel <ab.bell205@gmail.com>
2023-11-22 00:28:32 +0100

    added user model

---
+++ app/database/model.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+# define Base class for table class inheritance
+Base = declarative_base()
+
+
+# create a table for class state
+class User(Base):
+    '''
+        class defintion for SQL table states
+        pARAMETER
+            Base declarative
+        Return:
+        base meta data for the creation of state tables
+    '''
+    # define table name
+    __tablename__ = 'users'
+    # define column for users
+    id = Column(Integer, primary_key=True,
+                autoincrement=True, unique=True, nullable=False)
+    firstname = Column(String(256), nullable=False)
+    email = Column(String(256), nullable=False)
+    password = Column(String(256), nullable=False)
