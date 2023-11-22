b3b20479b3c0f3befc088e47139a608bf4f405a7
abel <ab.bell205@gmail.com>
2023-11-22 01:28:13 +0100

    added a password class

---
+++ app/auth/password.py
@@ -0,0 +1,9 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class Password:
+    
+    def hash(password:str):
+        return generate_password_hash(password=password)
+    
+    def verify_password(pwdhash, password):
+        return check_password_hash(pwhash=pwdhash, password=password)
