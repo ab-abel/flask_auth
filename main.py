39f8d55471d8da715396ba69f0ece84493b63a10
abel <ab.bell205@gmail.com>
2023-11-22 01:49:12 +0100

    added correct path

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask
 import os
 from dotenv import load_dotenv
-# from core.db import create_db
+from app.database.db import create_db
+from app.database.user import UserApi
 
 
 # load evironmental files
@@ -16,8 +17,14 @@ app = Flask(os.getenv('APP_NAME'),
 # from core.routes import bp as main_bp
 # app.register_blueprint(main_bp)
 
+# use the rest API
+
+
+
+
 # run the code
 if __name__ == '__main__':
     app.run(debug=True)
+    create_db()
     
  
