2bfc21524c4c0d691c1d9a3ca254e3349e725f0f
abel <ab.bell205@gmail.com>
2023-11-22 01:18:31 +0100

    add a REST API for user CRUD

--- app/database/user.py
+++ app/database/user.py
@@ -1,20 +1,66 @@
 from app.database.db import session
 from model import User
+from flask_restful import Resource, reqparse
 
-def create(data):
-    users = 
+# instantiate the parse method
+parser = reqparse.RequestParser()
 
-def view():
-    pass
+# get parsed response from form
+parser.add_argument('firstname', type=str)
+parser.add_argument('email', type=str)
+parser.add_argument('password', type=str)
 
-def show(id):
-    result = session.query(User).filter(User.id == id).first()
-    session.close()
-    return result
+# create a user api class
+class UserApi(Resource):
+    
+    # show a specific users
+    def show(self, id=None):
+        
+        # get a paticular users from DB
+        result = session.query(User).filter(User.id == id).first()
+        # close the db connection
+        session.close()
+        return result
 
-def update(id):
-    pass
+    # add a users to the db
+    def post(self):
+        
+        # get the parsed users param
+        args = parser.parse_args()
+        name = args['firstname']
+        email = args['email']
+        password = args['password']
+        
+        # hash the Password
+        
+        
+        # chwck if user already exit in the db
+        user = session.query(User).filter(User.email == email).first()
+        
+        # check if user exist
+        if user:
+            return {'msg': 'Username already exist'}
+        
+        # add the necw user a to he DB
+        new_user = User(name, email, password)
+        session.add(new_user)
+        session.commit()
+        session.close()
+    
+    def put(self, id):
+        args = parser.parse_args()
+        name = args['firstname']
+        email = args['email']
+        update_user = session.query(User).filter(User.id == id).first()
+        
+        if not update_user:
+            return {'msg':'Invalid user'}
+        
+        user = User(firstname=name, email=email)
+        session.add(user)
+        session.commit()
 
-def delete(id):
-    session.query(User).filter(User.id == id).delete()
-    session.commit()
+
+    def delete(self, id):
+        session.query(User).filter(User.id == id).delete()
+        session.commit()
