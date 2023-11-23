800ada2d5f5aad42e3e4b73d8986779ed826abda
abel <ab.bell205@gmail.com>
2023-11-23 15:44:44 +0100

    adding reg method

--- app/auth/register.py
+++ app/auth/register.py
@@ -12,11 +12,17 @@ class Registration():
     
     
     def register_user(self):
+        # validate inut
+        
+        # check if exist
+        
+        # pass to db
         
         pass
             
     def user_exist(email):
         user = session.query(User).filter(User.email == email).first()
+        session.close()
         if not user:
             return {'msg':"User already exist"}
         
