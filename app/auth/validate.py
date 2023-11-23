3224399518ed545180aaf26700e01a18cb075172
abel <ab.bell205@gmail.com>
2023-11-23 15:44:19 +0100

    convert validation to a class

--- app/auth/validate.py
+++ app/auth/validate.py
@@ -1,61 +1,77 @@
-# check for Integers
-def check_for_int(word):
-    a = []
-    for i in word:
-        if(i.isdigit()):
-            a.append(i)
-    if(len(a)>=1):
-        is_valid = True
-    else:
-        is_valid = False
-    return is_valid
+import re
 
-# check for white_space between words
-def check_white_space(word):
-    a = []
-    for i in word:
-        if(i.isspace()):
-            a.append(i)
-    if(len(a)>=1):
-        is_valid = True
-    else:
-        is_valid = False
-    return is_valid
+class Validation():
+    
+    def __init__(self):
+        self.is_valid = False
 
-# check for white
-def validate_password(password:str):
-    is_valid = False
-    if (len(password)>=8 and not password.islower() 
-        and not password.isupper() 
-        and check_for_int(password) 
-        and not check_white_space(password)):
+    # check for Integers
+    def check_for_int(self, word):
+        a = []
+        for i in word:
+            if(i.isdigit()):
+                a.append(i)
+        if(len(a)>=1):
+           self.s_valid = True
+        else:
+           self.is_valid = False
+        return self.is_valid
 
-        is_valid = True
-    else:
-        is_valid = False
-    return is_valid
+    # check for white_space between words
+    def check_white_space(self, word):
+        a = []
+        for i in word:
+            if(i.isspace()):
+                a.append(i)
+        if(len(a)>=1):
+           self.is_valid = True
+        else:
+           self.is_valid = False
+        return self.is_valid
 
-def detect_special_characters(pass_string:str):
-    import re
-    regex= re.compile('[@!#$%^&*()<>?/\\\|}{~:[\]`_]')
-    if(regex.search(pass_string) == None): 
-        res = False
-    else: 
-        res = True
-    return res
+    # check for white
+    def validate_password(self, password:str):
+        self.is_valid = False
+        if (len(password)>=8 and not password.islower() 
+            and not password.isupper() 
+            and self.check_for_int(password) 
+            and not self.check_white_space(password)):
 
-def detect_dashes(pass_string):
-    import re
-    regex= re.compile('[-]')
-    if(regex.search(pass_string) == None): 
-        res = False
-    else: 
-        res = True
-    return res
+           self.is_valid = True
+        else:
+           self.is_valid = False
+        return self.is_valid
 
-def check_if_empty(pass_string):
-    if not pass_string:
-        res = False
-    else:
-        res = True
-    return res
+    def detect_special_characters(self, pass_string:str):
+        import re
+        regex= re.compile('[@!#$%^&*()<>?/\\\|}{~:[\]`_]')
+        if(regex.search(pass_string) == None): 
+            self.is_valid = False
+        else: 
+            self.is_valid = True
+        return self.is_valid
+
+    def detect_dashes(self, pass_string):
+        import re
+        regex= re.compile('[-]')
+        if(regex.search(pass_string) == None): 
+            self.is_valid = False
+        else: 
+            self.is_valid = True
+        return self.is_valid
+
+    def check_if_empty(self, pass_string):
+        if not pass_string:
+            self.is_valid = False
+        else: 
+            self.is_valid = True
+        return self.is_valid
+
+    def validate_email(self, email):
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+        if re.fullmatch(regex, email):
+            self.is_valid = True
+        else:
+            self.is_valid = False
+        return self.is_valid
+        
