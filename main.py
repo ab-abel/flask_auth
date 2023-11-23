29c3c554d868a4f2a833f75a381d8662fa296580
abel <ab.bell205@gmail.com>
2023-11-23 01:18:36 +0100

    correct templates folder path added routes

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 from dotenv import load_dotenv
 from app.database.db import create_db
 from app.database.user import UserApi
+from flask_restful import Api
 
 
 # load evironmental files
@@ -10,8 +11,8 @@ load_dotenv()
 
 # instantiate the main app
 app = Flask(os.getenv('APP_NAME'),
-            template_folder='app/templates',
-            static_folder='app/static')
+            template_folder='templates',
+            static_folder='static')
 
 # load routes
 # from core.routes import bp as main_bp
@@ -19,8 +20,27 @@ app = Flask(os.getenv('APP_NAME'),
 
 # use the rest API
 
+api = Api(app)
 
 
+api.add_resource(UserApi,
+                 '/api/user/',
+                 '/api/user/<int:id>'
+                 )
+
+
+@app.route('/register', methods= ['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
+
+@app.route('/login', methods= ['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/')
+def home():
+    return render_template('/home.html')
 
 # run the code
 if __name__ == '__main__':
