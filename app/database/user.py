494522dda157bf5fc6013b6d18dc25a3a9a1dceb
abel <ab.bell205@gmail.com>
2023-11-23 01:19:22 +0100

    added response of type json

--- app/database/user.py
+++ app/database/user.py
@@ -2,14 +2,15 @@ from app.database.db import session
 from app.database.model import User
 from flask_restful import Resource, reqparse
 from app.auth.password import Password
+from flask import json, request
 
 # instantiate the parse method
 parser = reqparse.RequestParser()
 
 # get parsed response from form
-parser.add_argument('firstname', type=str)
-parser.add_argument('email', type=str)
-parser.add_argument('password', type=str)
+parser.add_argument('firstname', type=str, location='form')
+parser.add_argument('email', type=str,location='form')
+parser.add_argument('password', type=str, location='form')
 
 # create a user api class
 class UserApi(Resource):
@@ -21,7 +22,11 @@ class UserApi(Resource):
         result = session.query(User).filter(User.id == id).first()
         # close the db connection
         session.close()
-        return result
+        res ={
+            'name': result.fullname,
+            'email': result.email
+        }
+        return res
 
     # add a users to the db
     def post(self):
@@ -38,16 +43,23 @@ class UserApi(Resource):
         # chwck if user already exit in the db
         user = session.query(User).filter(User.email == email).first()
         
+        
         # check if user exist
         if user:
             return {'msg': 'Username already exist'}
         
         # add the necw user a to he DB
-        new_user = User(name, email, pswhash)
+        new_user = User(fullname=name, email=email, password=pswhash)
         session.add(new_user)
         session.commit()
         session.close()
+        res = {
+            'name': name,
+            'email': email
+            }
+        return json.dumps(res)
     
+
     def put(self, id):
         args = parser.parse_args()
         name = args['firstname']
@@ -57,7 +69,7 @@ class UserApi(Resource):
         if not update_user:
             return {'msg':'Invalid user'}
         
-        user = User(firstname=name, email=email)
+        user = User(fullname=name, email=email)
         session.add(user)
         session.commit()
 
