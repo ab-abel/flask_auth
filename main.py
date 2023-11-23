46277d01f0c06fb0d13e8870b68d74c534120131
abel <ab.bell205@gmail.com>
2023-11-24 00:18:31 +0100

    complete registration route method

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 from dotenv import load_dotenv
-from app.database.db import create_db
+from app.database.db import create_db, session
 from app.database.user import UserApi
 from flask_restful import Api
-
+from app.auth.register import Registration
+from app.auth.validate import Validation
+from app.database.model import User
 
 # load evironmental files
 load_dotenv()
@@ -31,7 +33,25 @@ api.add_resource(UserApi,
 
 @app.route('/register', methods= ['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    if request.method == 'POST':
+        fullname = request.form['firstname']
+        email = request.form['email']
+        password = request.form['password']
+        validate = Validation()
+        if not validate.is_empty(email) and \
+            not validate.is_empty(password) and \
+                not validate.is_empty(fullname) and \
+                    validate.validate_email(email):
+            user = session.query(User).filter(User.email == email).first()
+            if not user: 
+                resgister_class = Registration(fullname, email, password)
+                resgister_class.register_user()
+                return render_template('home.html')
+            else: 
+                return {'msg': 'User already exist'}, render_template('register.html')
+                        
+    else:
+        return render_template('register.html')
 
 
 @app.route('/login', methods= ['GET', 'POST'])
