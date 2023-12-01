589a3c7249086a4c3f9135d530605cce728ebaec
abel <ab.bell205@gmail.com>
2023-12-01 17:46:22 +0100

    modify getter and setter property for user model

--- app/database/model.py
+++ app/database/model.py
@@ -26,18 +26,19 @@ class User(db.Model, UserMixin):
        self.email = email
        self.password = password
    
-    
+    @property
     def is_active(self):
         """True, as all users are active."""
         return True
 
     def get_id(self):
         """Return the email address to satisfy Flask-Login's requirements."""
-        return self.email
+        return self.id
 
+    @property
     def is_authenticated(self):
         """Return True if the user is authenticated."""
-        return self.authenticated
+        return True
 
     def is_anonymous(self):
         """False, as anonymous users aren't supported."""
