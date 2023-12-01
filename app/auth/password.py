bb8cb7ec3676c261893daaf1106408dc19c0b3df
abel <ab.bell205@gmail.com>
2023-12-01 18:22:41 +0100

    added logout  and modify the run code

--- app/auth/password.py
+++ app/auth/password.py
@@ -5,5 +5,5 @@ class Password:
     def hash(password:str):
         return generate_password_hash(password=password)
     
-    def verify_password(pwdhash, password):
+    def verify_password(self, pwdhash, password):
         return check_password_hash(pwhash=pwdhash, password=password)
